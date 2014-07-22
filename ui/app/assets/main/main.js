8a7eba287699f967542fe5919170c8e58fa51423
Maxime DANTEC <hello@warry.fr>
2014-07-22 14:01:21 +0200

    Add new websocket and streams api

--- ui/app/assets/main/main.js
+++ ui/app/assets/main/main.js
@@ -3,11 +3,6 @@
  */
 require.config({
   baseUrl:  '/public',
-  paths: {
-    jquery: 'lib/jquery//jquery',
-    ko: 'lib/knockout/knockout',
-    ace: 'lib/ace/src/ace'
-  }
 });
 
 var vendors = [
@@ -15,8 +10,7 @@ var vendors = [
   'lib/knockout/knockout',
   'css',
   'text',
-  'lib/ace/src/ace',
-  'commons/visibility'
+  'lib/ace/src/ace'
 ]
 
 var commons = [
@@ -24,35 +18,44 @@ var commons = [
   'commons/effects',
   'commons/utils',
   'commons/settings',
-  'commons/streams',
-  'commons/events'
+  'commons/stream'
 ]
 
 var services = [
-    'services/oldSbt',
-    'services/sbt',
-    'services/build',
-    'services/log',
-    'services/tutorial',
-    'services/connection',
-    'widgets/notifications/notifications',
-    'services/typesafe'
 ]
 
-var core = [
-  'main/view',
-  'main/router',
-  'main/keyboard'
-]
 
+// DEBUG
+debug = true;
 require(vendors, function($, ko) {
   window.ko = ko; // it's used on every page...
   require(commons, function() {
-    require(services, function() {
-      require(core, function(view, router) {
-        view.render();
-        router.load(window.location.hash)
-      })
+    require(['commons/websocket'], function(WS) {
+      // PRINT EVERYTHING FROM WS
+      websocket
+        .subscribe()
+        .each(function(message){
+          console.log(message)
+        });
     })
   })
 })
+
+
+// var core = [
+//   'main/view',
+//   'main/router',
+//   'main/keyboard'
+// ]
+
+// require(vendors, function($, ko) {
+//   window.ko = ko; // it's used on every page...
+//   require(commons, function() {
+//     require(services, function() {
+//       require(core, function(view, router) {
+//         view.render();
+//         router.load(window.location.hash)
+//       })
+//     })
+//   })
+// })
