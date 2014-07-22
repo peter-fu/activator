bae388a67bf60bde05a09a7ca0372b5f54be2766
Maxime DANTEC <hello@warry.fr>
2014-07-22 15:37:41 +0200

    Update notification widget

--- ui/app/assets/widgets/notifications/notifications.js
+++ ui/app/assets/widgets/notifications/notifications.js
@@ -1,17 +1,16 @@
 /*
- Copyright (C) 2013 Typesafe, Inc <http://typesafe.com>
+ Copyright (C) 2014 Typesafe, Inc <http://typesafe.com>
  */
-define(['text!./notifications.html', 'lib/knockout/knockout', 'commons/widget', 'commons/utils', 'services/build'],
-    function(template, ko, Widget, utils, build) {
+define([
+  'text!./notifications.html',
+  'css!widgets/buttons/dropdown',
+  'css!./notifications'
+], function(
+  tpl
+){
 
-  var Notifications = utils.Class(Widget, {
-    id: 'notifications-widget',
-    template: template,
-    init: function() {
-      this.count = ko.computed(function() { return build.errors.all().length; });
-      this.items = build.errors.all;
-    }
-  });
+  var State = {}
 
-  return new Notifications();
-});
+  return bindhtml(tpl, State);
+
+})
