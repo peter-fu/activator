59d0ca4a5a3feef3a0ce7ba9ff8e10fef8f7d36d
Maxime DANTEC <hello@warry.fr>
2014-07-28 13:53:45 +0200

    bring new build plugin

---
+++ ui/app/assets/plugins/build/tasks/tasks.js
@@ -0,0 +1,29 @@
+define([
+  "services/sbt",
+  "main/plugins",
+  "text!./tasks.html",
+  "css!./tasks",
+  "css!widgets/modules/modules"
+], function(
+  sbt,
+  plugins,
+  tpl
+) {
+
+  var State = {
+    memoTaskScroll: ko.observable(),
+    memoLogsScroll: ko.observable(),
+    sbt: sbt,
+    clear: function() {
+      sbt.logs.logs([]);
+      sbt.tasks.executions.all([]);
+    }
+  };
+
+  return {
+    render: function(){
+      return bindhtml(tpl, State)
+    }
+  }
+
+});
