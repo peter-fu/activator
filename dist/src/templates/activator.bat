f7216b43724b2d806117f15b22835de26c391511
Henrik Engstrom <henrik.engstrom@gmail.com>
2014-08-05 17:06:49 -0400

    Revert "Ensure right java version is detected"
    This reverts commit 57cbdef685ee1e22cc4a6787b0fe283bdba21051.

--- dist/src/templates/activator.bat
+++ dist/src/templates/activator.bat
@@ -122,7 +122,7 @@ if "%JAVAOK%"=="false" (
 )
 
 rem Check what Java version is being used to determine what memory options to use
-for /f "tokens=3" %%g in ('"%_JAVACMD%" -version 2^>^&1 ^| findstr /i "version"') do (
+for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "version"') do (
     set JAVA_VERSION=%%g
 )
 
