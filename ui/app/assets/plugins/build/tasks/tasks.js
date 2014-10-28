b087b640183418fc9223703f6ff383b6e359b1ac
Havoc Pennington <hp@pobox.com>
2014-10-28 11:03:31 -0400

    On new log, append to rather than recreate tasks.logs
    This avoids n-squared complexity.

--- ui/app/assets/plugins/build/tasks/tasks.js
+++ ui/app/assets/plugins/build/tasks/tasks.js
@@ -19,21 +19,55 @@ define([
   function closeLogs() {
     window.location.hash = "#build/tasks/";
   }
-  var currentExecution = ko.observable();
+  var currentExecution = ko.observable(0);
 
   function scrollToBottom() {
     $(".logs")[0].scrollTop = 9e9;
     State.memoLogsScroll('stick');
   }
 
-  function selectedTask(m) {
-    return !currentExecution() || m.executionId == currentExecution();
+  function selectedExecution(m) {
+    return currentExecution() === 0 || m.executionId === currentExecution();
   }
 
-  var logs = ko.computed(function() {
-    sbt.app.settings.showLogDebug();
-    return sbt.logs.logs().filter(selectedTask);
-  })
+  // logs for the currently-selected execution.
+  var logs = ko.observableArray();
+  // id the logs above go with, so we know if it
+  // needs to be updated. 0 = unfiltered.
+  var logsExecutionId = 0;
+
+  function reloadLogsIfOutdated() {
+    // if currentExecution is 0, we show all logs
+    var currentId = currentExecution();
+    if (logsExecutionId != currentId) {
+      sbt.app.settings.showLogDebug();
+      // This is super-expensive, so it's vital NOT to do it
+      // every time there's a new log message (or we get
+      // N-squared complexity and performance fallover).
+      logsExecutionId = currentId;
+      logs(sbt.logs.logs().filter(selectedExecution));
+    }
+  }
+
+  currentExecution.subscribe(function(newCurrentExecution) {
+    reloadLogsIfOutdated();
+  });
+
+  // we know that the logs for a given task are append-only,
+  // so on each addition to the global logs, we can append
+  // to the task log. We can ignore deletions here because
+  // we never delete from the logs for a task.
+  // There's a Knockout Projections library which would
+  // do this for us more cleanly, but seems like overkill
+  // for the moment when we can do this simply.
+  sbt.logs.logs.subscribe(function(changes) {
+    changes.forEach(function(change) {
+      if (change.status === 'added') {
+        if (selectedExecution(change.value))
+          logs.push(change.value);
+      }
+    });
+  }, null, "arrayChange");
 
   var State = {
     memoTaskScroll: ko.observable(),
@@ -63,7 +97,7 @@ define([
       if (url.parameters){
         currentExecution(url.parameters[0])
       } else {
-        currentExecution(null)
+        currentExecution(0)
       }
     },
 
