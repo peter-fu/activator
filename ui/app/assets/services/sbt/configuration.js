78e622f05df1942ddc3e5eb2c65279aae9818b92
Henrik Engstrom <henrik.engstrom@gmail.com>
2015-01-28 10:22:55 -0500

    Updates after comments

--- ui/app/assets/services/sbt/configuration.js
+++ ui/app/assets/services/sbt/configuration.js
@@ -13,7 +13,6 @@ define([
   var backgroundRunPluginFileLocation = "/project/sbt-ui.sbt";
   var backgroundRunPluginFileContent = "// This plugin represents functionality that is to be added to sbt in the future\n\n" +
     "addSbtPlugin(\"com.typesafe.sbtrc\" % \"ui-interface-0-13\" % \"1.0-M1\")";
-
   var uiFileEchoSettings = "\n\nfork in run := true";
 
   // this file isn't required to exist, if it doesn't we should create
@@ -56,9 +55,14 @@ define([
   checkFileContent(serverAppModel.location+backgroundRunPluginFileLocation, backgroundRunPluginFileContent, function() {
     addedBackgroundFile(true);
   });
+
   checkFileContent(serverAppModel.location+buildFileLocation, uiFileEchoSettings, function() {
     addedForkInRun(true);
   }, true);
+
+  checkFileContent(serverAppModel.location+playForkRunPluginFileLocation, playForkRunPluginFileContent, function () {
+    addedPlayForkRun(true);
+  });
 
   var echoReady = ko.computed(function() {
     // TODO this is completely broken because applicationReady is probably true to begin with,
@@ -66,7 +70,7 @@ define([
     // be briefly true before we restart (when we want it to be true only after).
     // I think we should replace applicationReady with checking that the needed tasks are
     // present in the build.
-    return (tasks.applicationReady() && addedEchoFile() && addedBackgroundFile() && addedForkInRun());
+    return (tasks.applicationReady() && addedEchoFile() && addedBackgroundFile() && addedForkInRun() && addedPlayForkRun());
   });
 
   function echoInstalledAndReady(callback){
@@ -84,16 +88,9 @@ define([
     }
   }
 
-  checkFileContent(serverAppModel.location+playForkRunPluginFileLocation, playForkRunPluginFileContent, function () {
-    addedPlayForkRun(true);
-  });
-
   return {
     echoInstalledAndReady: echoInstalledAndReady,
     addedEchoFile:         addedEchoFile,
-    addedBackgroundFile:   addedBackgroundFile,
-    addedForkInRun:        addedForkInRun,
-    addedPlayForkRun:      addedPlayForkRun,
     echoReady:             echoReady
   };
 });
