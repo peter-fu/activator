2d4f55caf7fe8e4e667f18c956059a727f40a7e3
Maxime DANTEC <hello@warry.fr>
2015-02-02 14:08:46 +0100

    Fix regression in main classes event

--- ui/app/assets/main/main.js
+++ ui/app/assets/main/main.js
@@ -35,7 +35,8 @@ var core = [
 require(vendors, function($, ko) {
   window.ko = ko; // it's used on every page...
   require(commons, function() {
-    require(services, function(WS, sbt) {
+    require(services, function(sbt) {
+      window.sbt = sbt;
       require(core, function(view, router, WS) {
 
         view.render();
