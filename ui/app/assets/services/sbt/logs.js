b872276aac38c0950b9859be317d8f5c646fb371
Havoc Pennington <hp@pobox.com>
2015-02-19 18:55:55 -0500

    Count adjacent error lines as one in the counter.
    Also, go ahead and count TaskLogEvent errors too.

--- ui/app/assets/services/sbt/logs.js
+++ ui/app/assets/services/sbt/logs.js
@@ -26,32 +26,38 @@ define([
       return true;
   }
 
+  var countError = function(logs, m, which) {
+    if (m.event.entry && m.event.entry.level && m.event.entry.level === "error") {
+      // we get several error lines for each error, so we count each adjacent
+      // block of lines as one count. Counting three errors as one is better than
+      // counting one as three.
+      if (logs().length === 0 || (logs()[logs().length - 1].event.entry.level !== "error")) {
+        events.incrementCounters[which]();
+      }
+    }
+  };
+
   /**
   Logs, by execution/task
   */
   logEvent.matchOnAttribute("subType", "TaskLogEvent")
     .filter(filterDebug)
     .each(function(m) {
+      countError(logs, m, 'build');
       logs.push(m);
     });
 
   logEvent.matchOnAttribute("subType", "DetachedLogEvent")
     .filter(filterDebug)
     .each(function(m) {
-      // Increment the build error counter
-      if (m.event.entry && m.event.entry.level && m.event.entry.level === "error") {
-        events.incrementCounters.build();
-      }
+      countError(logs, m, 'build');
       logs.push(m);
     });
 
   logEvent
     .matchOnAttribute("subType", "BackgroundJobLogEvent")
     .each(function(m) {
-      // Increment the build error counter
-      if (m.event.entry && m.event.entry.level && m.event.entry.level === "error") {
-        events.incrementCounters.run();
-      }
+      countError(stdout, m, 'run');
       stdout.push(m);
     });
 
