4f3965d4ff23ce5d05aff94ce0f6e2d5a48188be
Maxime DANTEC <hello@warry.fr>
2015-02-24 11:33:35 +0100

    Remove news from welcome screen

--- ui/app/assets/plugins/welcome/welcome.js
+++ ui/app/assets/plugins/welcome/welcome.js
@@ -36,28 +36,6 @@ define([
     var self = {};
 
     self.appVersion = window.serverAppVersion
-    self.newsHtml = ko.observable('<div></div>');
-
-    self.loadNews = function() {
-      var areq = {
-        url: "http://downloads.typesafe.com/typesafe-activator/" + window.serverAppVersion + "/news.js",
-        type: 'GET',
-        // this is hardcoded for now since our server is just static files
-        // so can't respect a ?callback= query param.
-        jsonpCallback: 'setNewsJson',
-        dataType: 'jsonp' // return type
-      };
-      debug && console.log("sending ajax news request ", areq)
-      return $.ajax(areq);
-    }
-    self.setNewsJson = function(json) {
-      debug && console.log("setting news json to ", json);
-      if ('html' in json) {
-        this.newsHtml(json.html);
-      } else {
-        console.error("json does not have an html field");
-      }
-    }
 
     self.presentationMode = settings.observable("presentationMode", false);
 
@@ -76,7 +54,6 @@ define([
       return;
     });
 
-    self.loadNews();
     return self;
   }());
 
