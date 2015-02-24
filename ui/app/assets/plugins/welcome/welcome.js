de16f8642726e8844f41bf4385520c78013affaf
Maxime DANTEC <hello@warry.fr>
2015-02-24 11:24:46 +0100

    Fix presentation mode toggle

--- ui/app/assets/plugins/welcome/welcome.js
+++ ui/app/assets/plugins/welcome/welcome.js
@@ -3,16 +3,19 @@
  */
 define([
   'text!./welcome.html',
+  'commons/settings',
   'widgets/layout/layout',
   'widgets/layout/layoutManager',
   'css!./welcome.css'
 ], function(
   tpl,
+  settings,
   layout,
   layoutManager
 ){
 
-  var presentationModeStyle = function() {
+  var presentationModeStyle = (function() {
+    var added = false;
     // Create the <style> tag
     var style = document.createElement("style");
     // WebKit hack :(
@@ -20,8 +23,14 @@ define([
     // Add the <style> element to the page
     document.head.appendChild(style);
     style.sheet.insertRule(".presentation-mode .tutorial .page, .presentation-mode .build .logs, .presentation-mode .run .pluginBlock, .presentation-mode .code .editor, .presentation-mode .monitoring .tabs-step { zoom: 141% !important; }");
-    return style;
-  }
+
+    return function() {
+      if (!added) {
+        document.head.appendChild(style);
+        added = true;
+      }
+    }
+  }());
 
   var WelcomeState = (function(){
     var self = {};
@@ -50,14 +59,22 @@ define([
       }
     }
 
-    self.presentationMode = function(a,e) {
-      var on = e.target.checked;
-      document.body.style.zoom = on?"75%":"1";
-      document.body.classList.toggle("presentation-mode");
-      document.head.appendChild(presentationModeStyle());
+    self.presentationMode = settings.observable("presentationMode", false);
+
+    ko.computed(function() {
+      var on = self.presentationMode();
+      if (on) {
+        document.body.style.zoom = "75%";
+        document.body.classList.add("presentation-mode");
+        presentationModeStyle();
+      } else {
+        document.body.style.zoom = "1";
+        document.body.classList.remove("presentation-mode");
+      }
       layoutManager.panelOpenedSet(false);
       layoutManager.navigationOpened(!on);
-    }
+      return;
+    });
 
     self.loadNews();
     return self;
