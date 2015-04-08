21108bc8aee85ac42c8f12a5f2e1681602aee65b
Havoc Pennington <hp@pobox.com>
2015-04-08 17:25:43 -0400

    Fetch actual /partners/activator html for partners tab

--- ui/app/assets/plugins/partners/partners.js
+++ ui/app/assets/plugins/partners/partners.js
@@ -5,35 +5,45 @@ define([
   'main/plugins',
   'text!./partners.html',
   "widgets/layout/layout",
-  // replace this with proxy call
-  'text!./technology-partners.html',
   'css!./partners',
   "css!widgets/menu/menu"
 ], function(
   plugins,
   tpl,
-  layout,
-  partnerHtml
+  layout
 ){
 
   var State = {
     partnerHtml: ko.observable("<div class='hint'>Loading partner information...</div>")
   };
 
-  var domNodes = $.parseHTML(partnerHtml, null /* current document */, true /* keepScripts */);
-  var node = null;
-  try {
-    for (var i = 0; i < domNodes.length; ++i) {
-      if (domNodes[i].id === 'partner-wrapper')
-        node = $(domNodes[i]);
+  var onError = function() {
+    State.partnerHtml("<div class='hint'>Failed to load partner information from <a href='https://typesafe.com/partners/activator'>https://typesafe.com/partners/activator</a></div>");
+  };
+
+  var onGotHtml = function(data) {
+    var domNodes = $.parseHTML(data, null /* current document */, true /* keepScripts */);
+    var node = null;
+    try {
+      for (var i = 0; i < domNodes.length; ++i) {
+        if (domNodes[i].id === 'partner-wrapper')
+          node = $(domNodes[i]);
+      }
+    } catch (e) {
+      console.log("failed to find partner-wrapper node ", e);
     }
-  } catch (e) {
-    console.log("failed to find partner-wrapper node ", e);
-  }
-  if (node === null)
-    State.partnerHtml("<div class='hint'>Failed to load partner information from typesafe.com</div>");
-  else
-    State.partnerHtml(node.html());
+    if (node === null)
+      onError();
+    else
+      State.partnerHtml(node.html());
+  };
+
+  $.ajax({
+    url: '/api/proxy/partners/activator',
+    success: onGotHtml,
+    error: onError,
+    dataType: 'html'
+  });
 
   return {
     render: function(url){
